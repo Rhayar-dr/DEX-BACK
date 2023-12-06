e610db1ecc29d53e0f11eabee96cd595aef35df5
Rhayar Mascarello <rhayar.mascarello@gmail.com>
2023-12-06 19:10:20 -0300

    COMMIT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Moralis = require("moralis").default;
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
@@ -87,6 +87,6 @@ Moralis.start({
   apiKey: process.env.MORALIS_KEY,
 }).then(() => {
   app.listen(port, () => {
-    console.log(`Listening for API Calls`);
+    console.log(`Servidor rodando na porta ${port}`);
   });
 });
